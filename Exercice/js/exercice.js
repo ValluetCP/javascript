d775d13817fa74872152a68b0a0965985852ad11
CYNTHIA <cynthia.petitot@gmail.com>
2023-05-23 13:37:54 +0200

    Exercice_javascript

---
+++ Exercice/js/exercice.js
@@ -0,0 +1,28 @@
+
+                        //-- CHAÎNE DE CARACTERE --//
+
+var prenom = "Cynthia";  // string / chaîne de caractère
+
+var monNumero = 65; // numérique / number
+
+var monNum = "0616223915"; // string / chaîne de caractère
+
+//var nom = petitot;  // false / syntaxe fausse
+
+// ligne 1 :
+// le nom de la variable = prenom
+// la valeur = Cynthia
+// le type = string
+
+
+                        //-- BOOLEAN --//
+
+var booleanFalse = false; // boolean faux
+
+var booleanTrue = true; // boolean vrai
+
+var varNull = null; // null
+
+
+//--  syntaxe pour afficher le contenu (= valeur) de nos variables --//
+console.log(prenom);
