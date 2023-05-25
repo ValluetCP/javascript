6f3ec4c642ea4675635134e588535a9cf3e7a28c
CYNTHIA <cynthia.petitot@gmail.com>
2023-05-25 13:39:56 +0200

    Exercice 1 - correction

--- Exercice/js/exercice1.js
+++ Exercice/js/exercice1.js
@@ -6,23 +6,29 @@
 // 3.	Affecter le contenu du formulaire dans cette variable 
 // 4.	Saisir un entier dans le formulaire (sachant que quel que soit la valeur saisie dans le formulaire est de type string)
 // 5.	Déclarer une variable qui s’appelle nb2 et affecter le chiffre 7 à cette variable
-// 6.	Calculer le modulo de nb1 par nb2 
+// 6.	Calculer le modulo de nb1 par nb2 et affecter le résultat dans la variable nommée (‘nb3’ ou ‘resultat’)
 // 7.	Affichage du résultat sur la page web
 // 8.	Affichage du résultat sur la console
 // 9.	Affichage du résultat à partir d’une boîte de dialogue
+// 10.	Additionner (‘nb3’ ou ‘resultat’) avec nb2 et stocker le dans (‘nb3’ ou ‘resultat’) 
 
 
 // prompt();
-var nb1 = prompt("Saisir un entier dans le formulaire");
+var nb1 = prompt("Saisir un entier dans le formulaire"); // nb1 = 16
 var nb2 = 7;
-var resultat = nb1 % nb2;
+var resultat = nb1 % nb2; // resultat = 2
 
-document.write(resultat);
+document.write(resultat + " "); // 2
 console.log(resultat);
 alert(resultat);
 console.log(typeof resultat);
 
-resultat = resultat.toString();
-console.log(resultat);
-console.log(typeof resultat);
+// resultat = resultat.toString();
+// console.log(resultat);
+// console.log(typeof resultat);
+
+// resultat = resultat + nb2;
+resultat += nb2;
+document.write(resultat);
+
 
