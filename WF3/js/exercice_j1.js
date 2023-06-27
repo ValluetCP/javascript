94e646c2caebb5f3a0e5b27965befe09facb6830
CYNTHIA <cynthia.petitot@gmail.com>
2023-06-27 16:31:52 +0200

    WF3  Jour 1 - Exercice (part 2)

--- WF3/js/exercice_j1.js
+++ WF3/js/exercice_j1.js
@@ -25,19 +25,78 @@ ecrire un programme qui permet:
 3 d'afficher ado si age est compris entre 11 et 18
 4 d'afficher vieux si age age est strictement superieur a 18 */
 
-// ------------- Exercices - 2 -------------- // 
+// ------------- Exercices - 2 (avec if)-------------- // 
 
-let age = 2;
+// let age = 2;
 
-if(age < 5){
-    console.log("bébé");
+// if(age < 5){
+//     console.log("bébé");
+// }
+// else if((age >= 5) && (age <=10)){
+//     console.log("enfant");
+// }
+// else if((age > 10) && (age <=18)){
+//     console.log("ado");
+// }
+// else if(age > 18) {
+//     console.log("sage");
+// }
+
+
+// *****************  ENONCE - 3 **************//
+
+/* 
+3a/ En utilsant la structure conditionnelle switch
+ecrire un programme qui afficher le jour de la semaine en fontion d'un nombre donné si nombre = 0 afficher dimanche.....
+
+3b/ En ulisant la ternaire ecrire un programe qui affiche adulte si l'age est superieur ou egal a  18 et mineur si age est inferieur a 18.
+
+3c/  une annee bisextile est une anne qui est soit:
+1 - divisible par 4 sans etre divisible par 100
+soit:
+2 - divisible par 400
+
+*/
+
+// ------------- Exercices - 3 -------------- // 
+
+
+// Exercice 3a //
+
+var nombre = 2;
+// var nombre = prompt('écrivez un nombre entre 0 et 6');
+
+switch(nombre){
+    case 0:
+    console.log("dimanche");
+    break;
+    case 1:
+    console.log("lundi");
+    break;
+    case 2:
+    console.log("mardi");
+    break;
+    case 3:
+    console.log("mercredi");
+    break;
+    case 4:
+    console.log("jeudi");
+    break;
+    case 5:
+    console.log("vendredi");
+    break;
+    case 6:
+    console.log("samedi");
+    break;
 }
-else if((age >= 5) && (age <=10)){
-    console.log("enfant");
-}
-else if((age > 10) && (age <=18)){
-    console.log("ado");
-}
-else if(age > 18) {
-    console.log("sage");
-}
+
+
+// Exercice 3b //
+
+var age = 8;
+var votreAge = ( age <=  18) ? "mineur":"majeur";
+console.log(votreAge);
+
+
+// Exercice 3c //
+    
